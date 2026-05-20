65a1afb65931d0ab99500b0d3e709213f3bd0391
gamejuong-code <gamejuong@gmail.com>
2026-05-20 17:16:34 +0900

    Update base64 decoder.py

--- base64 decoder.py
+++ base64 decoder.py
@@ -1,9 +1,22 @@
 import base64
 
-text = input("Base64 입력: ")
+print("1. Encode")
+print("2. Decode")
+
+choice = input("선택: ")
+text = input("문자열 입력: ")
 
 try:
-    decoded = base64.b64decode(text).decode()
-    print("결과:", decoded)
+    if choice == "1":
+        encoded = base64.b64encode(text.encode()).decode()
+        print("인코딩 결과:", encoded)
+
+    elif choice == "2":
+        decoded = base64.b64decode(text).decode()
+        print("디코딩 결과:", decoded)
+
+    else:
+        print("잘못된 선택")
+
 except:
-    print("디코딩 실패")
+    print("변환 실패")
